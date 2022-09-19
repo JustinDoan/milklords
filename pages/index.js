f220855dc374b5819da90c78d847fabaff33dc30
Justin Doan <ivigideon25@gmail.com>
2022-09-18 21:34:19 -0400

    Milk Lords Initial Configuration

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-3xl">Milk Lords</h1>
+    </div>
+  )
+}
